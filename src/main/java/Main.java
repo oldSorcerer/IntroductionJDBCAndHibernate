dada82cdb222ae0fdd9a527d4ac890533f09b7a6
oldSorcerer <Echorupta0habibi0viki>
2023-01-22 13:24:48 +0300

    add main

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import dao.UserDaoJDBCImpl;
+import model.User;
+import util.Util;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Util.getConnection();
+        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+        userDao.createUsersTable();
+
+        userDao.saveUser("Елена", "Иванова", (byte) 20);
+        userDao.saveUser("Павел", "Захаров", (byte) 25);
+        userDao.saveUser("Мария", "Польских", (byte) 31);
+        userDao.saveUser("Дмитрий", "Васильев", (byte) 38);
+
+        List<User> allUsers = userDao.getAllUsers();
+        for (User user : allUsers) {
+            System.out.println(user);
+        }
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
+    }
+}
