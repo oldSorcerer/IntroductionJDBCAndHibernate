d2aa5e16cdde6b685297b1544d60be7ff715ec82
oldSorcerer <Echorupta0habibi0viki>
2023-01-22 13:30:15 +0300

    add lombok

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,5 +1,11 @@
 package model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
 public class User {
 
     public Long id;
@@ -10,52 +16,4 @@ public class User {
     public User() {
     }
 
-    public User(Long id, String name, String lastName, Byte age) {
-        this.id = id;
-        this.name = name;
-        this.lastName = lastName;
-        this.age = age;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Byte getAge() {
-        return age;
-    }
-
-    public void setAge(Byte age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", age=" + age +
-                '}';
-    }
 }
