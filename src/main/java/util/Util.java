e08b7bf3c9ce9711c2530bd508f26a9923c9c9f8
oldSorcerer <Echorupta0habibi0viki>
2023-01-22 13:24:30 +0300

    add util

---
+++ src/main/java/util/Util.java
@@ -0,0 +1,25 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/test";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+
+    public static Connection getConnection() {
+        Connection connection;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            connection.setAutoCommit(false);
+        } catch (SQLException e) {
+            System.out.println("Соедиение не установленно");
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+}
