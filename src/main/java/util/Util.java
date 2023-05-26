f6f8a25ffc8c3a5e269c9ccd033c8d56bdafde9d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2023-05-26 10:55:31 +0300

    refactor code

--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -15,7 +15,7 @@ import java.util.Properties;
 public class Util {
 
     private static final String URL = "jdbc:mysql://localhost:3306/test";
-    private static final String USERNAME = "root";
+    private static final String USER_NAME = "root";
     private static final String PASSWORD = "root";
 
     private static SessionFactory sessionFactory;
@@ -26,7 +26,7 @@ public class Util {
     public static Connection getJDBCConnection() {
         Connection connection;
         try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
             connection.setAutoCommit(false);
         } catch (SQLException e) {
             System.out.println("Соедиение не установленно");
@@ -43,7 +43,7 @@ public class Util {
                 Properties properties = new Properties();
                 properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
                 properties.put(Environment.URL, URL);
-                properties.put(Environment.USER, USERNAME);
+                properties.put(Environment.USER, USER_NAME);
                 properties.put(Environment.PASS, PASSWORD);
                 properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
                 properties.put(Environment.SHOW_SQL, "true");
